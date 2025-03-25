96e47dd06fe315b6a68bad7d109fe46b60970606
ShahJabir <shahjabirtaqi@gmail.com>
2025-03-26 01:44:29 +0600

    arguments logic added

---
+++ Torlia/Torlia.c
@@ -0,0 +1,15 @@
+/* Torlia.c */
+#include "torlia.h"
+
+int main( int argc, char *argv[])
+{
+	char* host;
+	int port;
+
+	if(argc < 3) {
+		fprintf(stderr, "Usage: %s <host> <port>\n", argv[0]);
+		return -1;
+	}
+
+}
+
---
+++ Torlia/torlia.h
@@ -0,0 +1,14 @@
+/* torlia.h */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#ifdef _WIN32
+#include <io.h>
+#else
+#include <unistd.h>
+#endif
+
+#define PROXY "127.0.0.1"
+#define PORT 9050
